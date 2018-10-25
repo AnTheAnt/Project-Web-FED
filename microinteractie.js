d04e73f5c2caeb474c9b0427077e7af8449e4172
Anna <annapyea@gmail.com>
2018-10-25 12:18:40 +0200

    test2

---
+++ microinteractie.js
@@ -0,0 +1,14 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser*/
+
+/*eslint 'no-console': 0*/
+
+console.log("Hello from myscript.js");
+
+
+var button = document.getElementById("onClick");
+
+button.addEventListener("click", function () {
+    alert("hoi");
+});
